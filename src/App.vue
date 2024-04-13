b223f9b22b49e4109e2168c156275a1e8e2d62f9
sck <sck@gmail.com>
2024-04-13 16:29:23 +0800

    form bind dishes

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <!-- <First />
   <Second class="active" />
   <TodoList /> -->
-  <EventDemo />
-
+  <!-- <EventDemo /> -->
+  <FormBind />
 </template>
 
 
 <script setup>
 // import TodoList from './components/TodoList.vue'
-import EventDemo from './components/EventDemo.vue';
+// import EventDemo from './components/EventDemo.vue';
+import FormBind from './components/FormBind.vue';
 // import First from "@/components/First.vue";
 // import Second from "@/components/Second.vue";
 
--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import './assets/global.css'
 createApp(App).use(ElementPlus).mount('#app')
