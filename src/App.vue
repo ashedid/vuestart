2a8b5ed5f3e76cdabedd0b48c17a91a787666907
sck <sck@gmail.com>
2024-04-13 15:23:15 +0800

    resend

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
-  <First/>
-  <Second class="active"/>
-  <TodoList/>
+  <!-- <First />
+  <Second class="active" />
+  <TodoList /> -->
+  <EventDemo />
+
 </template>
 
 
 <script setup>
-import TodoList from './components/TodoList.vue'
-
-import First from "@/components/First.vue";
-import Second from "@/components/Second.vue";
+// import TodoList from './components/TodoList.vue'
+import EventDemo from './components/EventDemo.vue';
+// import First from "@/components/First.vue";
+// import Second from "@/components/Second.vue";
 
 
 
 </script>
-<style>
-
-
-</style>
+<style></style>
