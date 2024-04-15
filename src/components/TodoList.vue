89fbab74d0d3aafc3c3e5cb765797b776d4ee36e
sck <sck@gmail.com>
2024-04-15 12:50:30 +0800

    todolist fix 2

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 
 
 const todos = ref([
@@ -20,23 +20,33 @@ let nextTodoId = todos.value.length + 1
 const newTodoText = ref('')
 
 function addNewTodo() {
-  todos.value.push({ id: nextTodoId++, title: newTodoText.value })
+  todos.value.push({id: nextTodoId++, title: newTodoText.value})
   newTodoText.value = ''
   console.log(todos.value)
 }
 </script>
 
 <template>
-  <el-form v-on:submit.prevent="console.log(addNewTodo)">
-    <label for="new-todo">Add a todo</label>
-    <el-input v-model="newTodoText" id="new-todo" placeholder="E.g. Feed the cat" />
-    <el-button @click="addNewTodo">Add</el-button>
+  <el-form @submit.prevent="addNewTodo">
+    <!--    <label for="new-todo">Add a todo</label>-->
+    <el-input style="max-width: 480px" v-model="newTodoText" id="new-todo" placeholder="E.g. Feed the cat"/>
+
+    <el-button style="margin-left: 4px" type="primary" @click="addNewTodo">Add</el-button>
   </el-form>
-  <ul>
-    <li v-for="(todo, index) in todos" :key="todo.id" :title="todo.title"> {{ todo.title }}
+
+  <el-card style="max-width: 480px">
+
+    <p  class="todo-item" v-for="(todo, index) in todos" :key="todo.id" :title="todo.title"> {{ todo.title }}
       <el-button @click="todos.splice(index, 1)">Remove</el-button>
-    </li>
-  </ul>
+    </p>
+    <!--    <template #footer>Footer content</template>-->
+  </el-card>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}</style>
